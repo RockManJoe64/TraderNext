ebf37d8b30d7ae717e15923704a724579d9daeaa
Jose <jose.e.chavez@gmail.com>
2021-05-25 20:52:30 -0500

    Created IAuditable interface

---
+++ TraderNext.Core/Models/IAuditable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TraderNext.Core.Models
+{
+    public interface IAuditable
+    {
+        DateTime CreatedTime { get; set; }
+
+        long CreatedBy { get; set; }
+
+        DateTime ModifiedTime { get; set; }
+
+        long ModifiedBy { get; set; }
+    }
+}
